7032892f3b118da413d1f0ade1ed99b9e853991f
KATOR2 <xyh49@outlook.com>
2024-03-17 13:54:25 +0800

    Error Handing
    Error Handing

---
+++ Error_Handling/src/dividedbyzero/Main.java
@@ -0,0 +1,12 @@
+package dividedbyzero; 
+ 
+public class Main { 
+ 
+ /** 
+ * @param args the command line arguments 
+ */ 
+ public static void main(String[] args) { 
+ System.out.println(3/0); 
+ } 
+ 
+}
---
+++ Error_Handling/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Error_Handling {
+}
