d5c4468109a4100c7b61cb5f58928d407af3b7e0
KATOR2 <xyh49@outlook.com>
2024-03-17 14:04:02 +0800

    Error Handing

--- Error_Handling/src/dividedbyzero/Main.java
+++ Error_Handling/src/dividedbyzero/Main.java
@@ -1,12 +1,16 @@
-package dividedbyzero; 
- 
-public class Main { 
- 
- /** 
- * @param args the command line arguments 
- */ 
- public static void main(String[] args) { 
- System.out.println(3/0); 
- } 
- 
+package dividedbyzero;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            System.out.println(3 / 0);
+        } catch (Exception e) {
+            System.out.printf("Caught runtime exception = %s\n", e.getMessage());
+        }
+    }
+
 }
