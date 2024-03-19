cbeb088d413d6c3ef3d221c1827e4cd8c7a8133b
KATOR2 <xyh49@outlook.com>
2024-03-19 15:30:03 +0800

    try/catch block added

--- Error_Handling/src/dividedbyzero/Main.java
+++ Error_Handling/src/dividedbyzero/Main.java
@@ -1,4 +1,4 @@
-package dividebyzero;
+package dividedbyzero;
 
 public class Main {
 
@@ -6,7 +6,11 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println(3/0);
+        try {
+            System.out.println(3 / 0);
+        } catch (ArithmeticException e) { // Spécifie que l'exception à capturer est ArithmeticException
+            System.out.printf("Caught arithmetic exception = %s\n", e.getMessage());
+        }
     }
 
-} 
+}
