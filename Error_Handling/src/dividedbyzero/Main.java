fba5ca1a488b2132c2cabd83ef8b7ff00d2e1b0d
KATOR2 <xyh49@outlook.com>
2024-03-19 15:22:20 +0800

    try/catch block added

--- Error_Handling/src/dividedbyzero/Main.java
+++ Error_Handling/src/dividedbyzero/Main.java
@@ -1,4 +1,4 @@
-package dividedbyzero;
+package dividebyzero;
 
 public class Main {
 
@@ -6,11 +6,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        try {
-            System.out.println(3 / 0);
-        } catch (ArithmeticException e) { // 指定要捕获的异常为ArithmeticException
-            System.out.printf("Caught arithmetic exception = %s\n", e.getMessage());
-        }
+        System.out.println(3/0);
     }
 
-}
+} 
